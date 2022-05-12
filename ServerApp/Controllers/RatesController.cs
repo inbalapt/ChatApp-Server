7f333422c6c9a6cfeff90f0b5ca0bd6b6c96852b
Inbal Apt <inbalapt@gmail.com>
2022-05-12 14:12:16 +0300

    h

---
+++ ServerApp/Controllers/RatesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerApp.Models;
+
+namespace ServerApp.Controllers
+{
+    public class RatesController : Controller
+    {
+        private static List<Rate> rates = new List<Rate>();
+        public IActionResult Index()
+        {
+            return View(rates);
+        }
+    }
+}
---
+++ ServerApp/Models/Rate.cs
@@ -0,0 +1,10 @@
+﻿namespace ServerApp.Models
+{
+    public class Rate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Feedback { get; set; }
+        public int Rating { get; set; }
+    }
+}
