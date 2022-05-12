0c0e4113afea50c4615008266de16e75b75b2efb
noaLeshem2 <noaleshem99@gmail.com>
2022-05-12 16:43:14 +0300

    Description

--- ServerApp/Controllers/RatesController.cs
+++ ServerApp/Controllers/RatesController.cs
@@ -10,9 +10,9 @@ namespace ServerApp.Controllers
         public RatesController()
         {
             rates = new List<Rate>();
-            rates.Add(new Rate() { Id = 1, Name = "Noa", Feedback = "good", Rating = 5 });
-            rates.Add(new Rate() { Id = 2, Name = "Inbal", Feedback = " ho god", Rating = 4 });
-            rates.Add(new Rate() { Id = 3, Name = "Amit", Feedback = "terrible", Rating = 3 });
+            rates.Add(new Rate() { Id = 1, Name = "Noa", Description = "good", Rating = 5 });
+            rates.Add(new Rate() { Id = 2, Name = "Inbal", Description = " ho god", Rating = 4 });
+            rates.Add(new Rate() { Id = 3, Name = "Amit", Description = "terrible", Rating = 3 });
         }
         public IActionResult Index()
         {
