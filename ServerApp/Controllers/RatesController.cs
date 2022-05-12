19feb759d3881cbfc429ec5e88953e56bb85ceb6
Inbal Apt <inbalapt@gmail.com>
2022-05-12 14:35:21 +0300

    updates

--- ServerApp/Controllers/RatesController.cs
+++ ServerApp/Controllers/RatesController.cs
@@ -5,7 +5,15 @@ namespace ServerApp.Controllers
 {
     public class RatesController : Controller
     {
-        private static List<Rate> rates = new List<Rate>();
+        private static List<Rate> rates;
+
+        public RatesController()
+        {
+            rates = new List<Rate>();
+            rates.Add(new Rate() { Id = 1, Name = "Noa", Feedback = "good", Rating = 5 });
+            rates.Add(new Rate() { Id = 2, Name = "Inbal", Feedback = " ho god", Rating = 4 });
+            rates.Add(new Rate() { Id = 3, Name = "Amit", Feedback = "terrible", Rating = 3 });
+        }
         public IActionResult Index()
         {
             return View(rates);
