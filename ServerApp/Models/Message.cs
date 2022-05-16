b57fb291add55f6bb3d18eb14e58ae577757fa40
noaLeshem2 <noaleshem99@gmail.com>
2022-05-16 19:08:36 +0300

    message

---
+++ ServerApp/Models/Message.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServerApp.Models
+{
+    public class Message
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Content")]
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name = "Server")]
+        public string Server { get; set; }
+
+        [Required]
+        [Display(Name = "Created")]
+        public string Created { get; set; }
+
+        [Required]
+        [Display(Name = "From")]
+        public Contact From { get; set; }
+
+        [Required]
+        [Display(Name = "To")]
+        public Contact To { get; set; } 
+
+
+
+    }
+}
