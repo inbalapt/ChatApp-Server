8270a5253d301375d7e727a46f225ca0ca2a43d8
Inbal Apt <inbalapt@gmail.com>
2022-05-17 13:59:38 +0300

    n

---
+++ ServerApi/Models/Contacts.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServerApp.Models
+{
+    public class Contacts
+    {
+        [Display(Name = "Id")]
+        public string Id { get; set; }
+
+        [Display(Name = "Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Server")]
+        [Required]
+        public string Server { get; set; }
+
+        [Display(Name = "Last")]
+        public string Last { get; set; }
+
+        [Display(Name = "LastDate")]
+        public int LastDate { get; set; }
+
+
+    }
+}
